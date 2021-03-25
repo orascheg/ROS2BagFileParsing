c58ffaa69a040e0f178244a1664f5d555c30d1bd
Gerbert Orasche <gerbert.orasche@avl.com>
2021-03-25 15:10:31 +0100

    now got the right dev_cpp_node.cpp

--- dev_ws/src/dev_cpp_pkg/src/dev_cpp_node.cpp
+++ dev_ws/src/dev_cpp_pkg/src/dev_cpp_node.cpp
@@ -5,27 +5,28 @@
 #include <iostream>
 
 // ROS
-#include "rosbag2/sequential_reader.hpp"
-#include "rosbag2/typesupport_helpers.hpp"
-#include "rosbag2/converter_interfaces/serialization_format_converter.hpp"
+#include "rosbag2_cpp/readers/sequential_reader.hpp"
+#include "rosbag2_cpp/typesupport_helpers.hpp"
+#include "rosbag2_cpp/converter_interfaces/serialization_format_converter.hpp"
+#include "rosbag2_cpp/types/introspection_message.hpp"
 
 // ROS MSG
 #include "dev_cpp_pkg/msg/gps_rx.hpp"
 
-using rosbag2::converter_interfaces::SerializationFormatConverter;
+using rosbag2_cpp::converter_interfaces::SerializationFormatConverter;
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
   
-  rosbag2::SequentialReader reader;
-  rosbag2::StorageOptions storage_options{};
+  rosbag2_cpp::readers::SequentialReader reader;
+  rosbag2_cpp::StorageOptions storage_options{};
   
   storage_options.uri = "../rosbag2_test_data";
   storage_options.storage_id = "sqlite3";
 
-  rosbag2::ConverterOptions converter_options{};
+  rosbag2_cpp::ConverterOptions converter_options{};
   converter_options.input_serialization_format = "cdr";
   converter_options.output_serialization_format = "cdr";
   reader.open(storage_options, converter_options);
@@ -47,15 +48,16 @@ int main(int argc, char ** argv)
     
     // deserialization and conversion to ros message
     dev_cpp_pkg::msg::GpsRx msg;
-    auto ros_message = std::make_shared<rosbag2_introspection_message_t>();
+    auto ros_message = std::make_shared<rosbag2_cpp::rosbag2_introspection_message_t>();
     ros_message->time_stamp = 0;
     ros_message->message = nullptr;
     ros_message->allocator = rcutils_get_default_allocator();
     ros_message->message = &msg;
-    auto type_support = rosbag2::get_typesupport("dev_cpp_pkg/msg/GpsRx", "rosidl_typesupport_cpp");
-
-    rosbag2::SerializationFormatConverterFactory factory;
-    std::unique_ptr<rosbag2::converter_interfaces::SerializationFormatDeserializer> cdr_deserializer_;
+    auto library = rosbag2_cpp::get_typesupport_library("dev_cpp_pkg/msg/GpsRx", "rosidl_typesupport_cpp");
+    auto type_support = rosbag2_cpp::get_typesupport_handle("dev_cpp_pkg/msg/GpsRx", "rosidl_typesupport_cpp", library);
+    
+    rosbag2_cpp::SerializationFormatConverterFactory factory;
+    std::unique_ptr<rosbag2_cpp::converter_interfaces::SerializationFormatDeserializer> cdr_deserializer_;
     cdr_deserializer_ = factory.load_deserializer("cdr");
     
     cdr_deserializer_->deserialize(serialized_message, type_support, ros_message);
